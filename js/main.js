0dd9472d8ac6fbf844c2db2becfdfac8e8d8f0f1
Michelle <vand0432@algonquinlive.com>
2016-02-23 17:40:30 -0500

    indenting and stuff

--- js/main.js
+++ js/main.js
@@ -1,30 +1,52 @@
-var colour = prompt("Let's make a story together!  Enter a colour");
-var creature = prompt("What is your favourite creature?");
-var adjective = prompt("choose an adjective");
-var verb = prompt("choose a past tense verb (ran, kissed etc)");
-var story = prompt("Which story would you like? Enter the letter below: a)snacking b)escaping c)winning");
-
+var colour;
+var creature;
+var adjective;
+var verb;
+var story;
 
 var storyA = function () {
-  document.write("After snacking on" + " " + advective + "treats, the" + colour +" bellied" + " " + creature + verb + "for hours.");
+
+  document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verb + ' for hours.');
 };
 
 var storyB = function () {
-  document.write("Ameliea" + verb + " through the " + adjective + colour + " nebula escaping the space " creature);
-}
+  document.write('Ameilia ' + verb + ' through the ' + adjective + ' ' + colour + ' nebula escaping the space ' +  creature + '.');
+};
 
 var storyC = function () {
-  document.write("Jackson chose his " + adjective + ", " + colour + " " + creature + "and " verb + "it to the table knowing he won.")
-}
+  document.write('Jackson chose his ' + adjective + ', ' + colour + ' ' + creature + 'and ' + verb + 'it to the table knowing he won.');
+};
 
-if (story == a){
-  storyA;
-}
+alert('Let’s make a story together!');
 
-if (story == a){
-  storyB;
-}
+colour = prompt('Enter a colour');
+creature = prompt('What\'s your favourite creature?');
+adjective = prompt('choose an adjective');
+verb = prompt('choose a past tense verb (ran, kissed etc)');
+story = prompt('Which story would you like?\nEnter the letter below:\na)snacking\nb)escaping\nc)winning');
 
-if (story == a){
-  storyC;
+if (story == 'a' || story == 'A') {
+  storyA();
+} else if (story == 'b' || story == 'B') {
+  storyB();
+} else if (story == 'c' || story == 'C') {
+  storyC();
+} else {
+  document.write('You are no fun.');
+};
+
+/*
+switch (story) {
+  case 'a':
+  case 'A':
+    break;
+  case 'b':
+    break;
+    "//n"
+    "Let's make a story together!"
+    "A colour"
+    "A creature"
+    "An adjective"
+    "A past tense verb"
 }
+*/
