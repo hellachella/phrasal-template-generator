f5d78ef5f3c548e366a1ef6c1a9988c5db7d386d
Michelle <vand0432@algonquinlive.com>
2016-02-23 16:44:06 -0500

    If statements and functions etc

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
 var colour = prompt("Let's make a story together!  Enter a colour");
 var creature = prompt("What is your favourite creature?");
 var adjective = prompt("choose an adjective");
-var verb = prompt("choose a verb (running, kissing, etc)");
+var verb = prompt("choose a past tense verb (ran, kissed etc)");
 var story = prompt("Which story would you like? Enter the letter below: a)snacking b)escaping c)winning");
 
 
+var storyA = function () {
+  document.write("After snacking on" + " " + advective + "treats, the" + colour +" bellied" + " " + creature + verb + "for hours.");
+};
+
+var storyB = function () {
+  document.write("Ameliea" + verb + " through the " + adjective + colour + " nebula escaping the space " creature);
+}
+
+var storyC = function () {
+  document.write("Jackson chose his " + adjective + ", " + colour + " " + creature + "and " verb + "it to the table knowing he won.")
+}
+
+if (story == a){
+  storyA;
+}
+
+if (story == a){
+  storyB;
+}
+
+if (story == a){
+  storyC;
+}
