83637936c9e84977c2db6ac2c2000313abc7d5cb
Michelle <vand0432@algonquinlive.com>
2016-02-23 16:35:56 -0500

    Set up variables

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var colour = prompt("Let's make a story together!  Enter a colour");
+var creature = prompt("What is your favourite creature?");
+var adjective = prompt("choose an adjective");
+var verb = prompt("choose a verb (running, kissing, etc)");
+var story = prompt("Which story would you like? Enter the letter below: a)snacking b)escaping c)winning");
+
+
